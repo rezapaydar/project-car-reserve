e292deff23e4f3c6ca362a3a725252a5c62fba5a
rezajaberi <rezapaydarhmd@gmail.com>
2020-08-10 05:58:27 -0700

    web site ended but have bug

--- project website car reserve/js/scripts.js
+++ project website car reserve/js/scripts.js
@@ -5,3 +5,17 @@
 //         }, 1500);
 //     }
 // })
+// let scroll = window.scrollY;
+let imgcont = document.getElementById('img-cont');
+document.addEventListener('scroll', function () {
+    if (window.pageYOffset < 800 || window.pageYOffset > 2000) {
+    //    alert(window.pageYOffset)
+        $('img[alt="bg-down"]').css("display","none")
+    }
+    else {
+        
+        $('img[alt="bg-down"]').css("display","block")
+
+
+    }
+})
